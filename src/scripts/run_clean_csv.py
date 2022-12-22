c229f4f20defc7a1e7d63338ef9d213febc3859e
nemishmehta <23.nemishmehta@gmail.com>
2022-12-22 18:10:37 +0100

    Refactored code to check if file path exists before passing it.

--- src/scripts/run_clean_csv.py
+++ src/scripts/run_clean_csv.py
@@ -1,8 +1,22 @@
-import sys
+import argparse
+from pathlib import Path
 
 from src.main.clean_csv import CleanCsv
 
 if __name__ == "__main__":
-    file_path = sys.argv[1]
-    clean_csv_file = CleanCsv(file_path)
-    clean_csv_file.run_pipeline()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path")
+    args = parser.parse_args()
+    file_path = Path(args.file_path)
+
+    if file_path.exists():
+        clean_csv_file = CleanCsv(file_path)
+        clean_csv_file.run_pipeline()
+    else:
+        print(
+            (
+                "The given filepath is incorrect. Please check if this "
+                f"filepath exists - {file_path}"
+            )
+        )
