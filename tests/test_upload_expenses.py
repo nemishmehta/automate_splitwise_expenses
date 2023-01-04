943eab888c890e46df82628ffa6f065dd19fd84b
nemishmehta <23.nemishmehta@gmail.com>
2023-01-04 15:30:25 +0100

    Added tests for choosing group and friend.

--- tests/test_upload_expenses.py
+++ tests/test_upload_expenses.py
@@ -59,40 +59,107 @@ def user_groups_members():
     }
 
 
-@pytest.mark.parametrize("val_list", [("", 4), ("", "a", 4), ("", 3, 4)])
+@pytest.mark.parametrize(
+    "user_input, expected_result",
+    [(("", 4), 12035391), (("", "a", 4), 12035391), (("", 3, 4), 12035391)],
+)
 def test_choose_personal_expense_group(
     upload_expense_class,
     user_groups,
     user_groups_members,
     user_id,
     monkeypatch,
-    val_list,
+    user_input,
+    expected_result,
 ):
-    inputs = iter(val_list)
+    inputs = iter(user_input)
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     assert (
         upload_expense_class.choose_personal_expense_group(
             user_groups, user_groups_members, user_id
         )
-        == 12035391
+        == expected_result
     )
 
 
-@pytest.mark.parametrize("val_list", [" ", "s", "4", " "])
+@pytest.mark.parametrize(
+    "user_input, expected_result",
+    [(" ", None), ("s", None), ("4", None), (" ", None)],
+)
 def test_choose_personal_expense_group_no_group(
     upload_expense_class,
     user_groups,
     user_groups_members,
     user_id,
     monkeypatch,
-    val_list,
+    user_input,
+    expected_result,
 ):
 
-    inputs = iter(val_list)
+    inputs = iter(user_input)
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     assert (
         upload_expense_class.choose_personal_expense_group(
             user_groups, user_groups_members, user_id
         )
-        is None
+        is expected_result
+    )
+
+
+@pytest.mark.parametrize(
+    "user_input_groups, personal_expense_group_id, expected_result",
+    [
+        ("1", 12035391, ("Restaurant", 34894512)),
+        (("0", "1"), 12035391, ("Restaurant", 34894512)),
+        ("4", 12035391, ("Personal", 12035391)),
+        (("s", "4"), 12035391, ("Personal", 12035391)),
+        (("6", "4"), 12035391, ("Personal", 12035391)),
+        (("4", "2"), None, ("Home", 20340193)),
+        (("0", "-2", "2"), None, ("Home", 20340193)),
+    ],
+)
+def test_choose_group(
+    upload_expense_class,
+    user_groups,
+    user_groups_members,
+    user_input_groups,
+    personal_expense_group_id,
+    expected_result,
+    monkeypatch,
+):
+    inputs = iter(user_input_groups)
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    assert (
+        upload_expense_class.choose_group(
+            personal_expense_group_id, user_groups, user_groups_members
+        )
+        == expected_result
+    )
+
+
+@pytest.mark.parametrize(
+    "group_id, user_input_friends, expected_result",
+    [
+        (20340193, "1", ("Tom", 82514972)),
+        (20340193, ("0", "2"), ("Linda", 25087341)),
+        (12349123, ("0", "-1", "2"), ("George", 39083412)),
+    ],
+)
+def test_choose_friend(
+    upload_expense_class,
+    user_groups_members,
+    user_id,
+    user_friends,
+    group_id,
+    user_input_friends,
+    expected_result,
+    monkeypatch,
+):
+    inputs = iter(user_input_friends)
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    assert (
+        upload_expense_class.choose_friend(
+            group_id, user_groups_members, user_id, user_friends
+        )
+        == expected_result
     )
